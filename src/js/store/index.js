0aa41506f6e78f1e35cc62ebfe9c2412a2808da5
Yatin Burhmi <yatin.burhmi@kuliza.com>
2020-12-09 15:47:27 +0530

    Using middleware function

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,10 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/index';
+import { forbiddenWordsMiddleware } from "../middleware";
 
 // store is the result of calling createStore and it takes reducer as first argument
 // you may also pass initial state to createStore
 
 //  STATE IN REDUX COMES FROM REDUCERS // REDUCERS PRODUCE THE STATE OF YOUR APPLICATION
-const store = createStore(rootReducer); 
+const store = createStore(
+    rootReducer,
+    applyMiddleware(forbiddenWordsMiddleware)); 
 
 export default store;
